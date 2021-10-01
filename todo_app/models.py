dfb39fd101df50dddaf21e5196800a3661d098f4
Danyal Basar <dboy7098@gmail.com>
2021-10-01 12:34:03 +0530

    Update models.py

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class TasksModel(models.Model):
-	task = models.CharField(max_length=150)
+	user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+	task = models.TextField()
 	completed = models.BooleanField(default=False)
 	created = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.task
 
-class FbModel(models.Model):
+class FeedbackModel(models.Model):
 	name = models.CharField(max_length=30)
-	email = models.CharField(max_length=30)
+	email = models.EmailField()
 	feedback = models.TextField()
 	created = models.DateTimeField(auto_now_add=True)
