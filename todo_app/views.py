2dd2ebd6ed77953214bb7381f4224843fc19746c
Danyal Basar <dboy7098@gmail.com>
2021-10-01 12:34:30 +0530

    Update views.py

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 from django.core.mail import send_mail
 from random import randrange
-from django.urls import reverse
-from .forms import TasksForm, FbForm
-from .models import TasksModel, FbModel
+from .forms import TasksForm, FeedbackForm
+from .models import TasksModel, FeedbackModel
 
-def usignup(request):
+def user_signup(request):
 	if request.method == "POST":
+		user_name = request.POST.get("user_name")
 		em = request.POST.get("em")
 		try:
-			usr = User.objects.get(username=em)
-			return render(request, "usignup.html", {"msg":"Already Registered"})
+			user = User.objects.get(email=em)
+			return render(request, "user_signup.html", {"msg":"Email already registered"})
 		except User.DoesNotExist:
 			pw = ""
 			text = "abcdefghijklmnopqrstuvwxyz0123456789"
@@ -23,30 +24,31 @@ def usignup(request):
 			host = 'mail.task.webapp@gmail.com'
 			recepient = [em]
 			send_mail(subject, msg, host, recepient)
-			usr = User.objects.create_user(username=em, password=pw)
-			usr.save()
-			return redirect("ulogin")
+			user = User.objects.create_user(username=user_name, email=em, password=pw)
+			user.save()
+			return redirect("user_login")
 	else:
-		return render(request, "usignup.html")
+		return render(request, "user_signup.html")
 
-def ulogin(request):
+def user_login(request):
 	if request.method == "POST":
-		em = request.POST.get("em")
+		user_name = request.POST.get("user_name")
 		pw = request.POST.get("pw")
-		usr = authenticate(username=em, password=pw)
-		if usr is not None:
-			login(request, usr)
+		user = authenticate(username=user_name, password=pw)
+		if user is not None:
+			login(request, user)
 			return redirect("home")
 		else:
-			return render(request, "ulogin.html", {"msg":"Invalid Login"})
+			return render(request, "user_login.html", {"msg":"Invalid Login"})
 	else:
-		return render(request, "ulogin.html")
+		return render(request, "user_login.html")
 
-def uforgotpass(request):
+def user_forgotpass(request):
 	if request.method == "POST":
+		#user_name = request.POST.get("username")
 		em = request.POST.get("em")
 		try:
-			usr = User.objects.get(username=em)
+			user = User.objects.get(email=em)
 			pw = ""
 			text = "abcdefghijklmnopqrstuvwxyz0123456789"
 			for i in range(8):
@@ -56,68 +58,73 @@ def uforgotpass(request):
 			host = 'mail.task.webapp@gmail.com'
 			recepient = [em]
 			send_mail(subject, msg, host, recepient)
-			usr.set_password(pw)
-			usr.save()
-			return redirect("ulogin")
+			user.set_password(pw)
+			user.save()
+			return redirect("user_login")
 		except User.DoesNotExist:
-			return render(request, "uforgotpass.html", {"msg":"Not Registered"})
+			return render(request, "user_forgotpass.html", {"msg":"Not Registered"})
 	else:
-		return render(request, "uforgotpass.html")
+		return render(request, "user_forgotpass.html")
 
-def ulogout(request):
+def user_logout(request):
 	logout(request)
-	return redirect("ulogin")
+	return redirect("user_login")
 
 def home(request):
 	if request.user.is_authenticated:
 		return render(request, "home.html")
 	else:
-		return redirect("ulogin")
+		return redirect("user_login")
+
+@login_required(login_url="user_login")
 def create(request):
 	if request.method == "POST":
-		task = TasksForm(request.POST)
-		if task.is_valid():
-			task.save()
-			fm = TasksForm()
-			return render(request, "create.html", {"fm":fm, "msg":"Task Added"})
-		else:
-			return render(request, "create.html", {"fm":task, "msg":"Check Issue"})
+		#task_data = TasksForm(request.POST)
+		task = request.POST['task']    
+		task_data = TasksModel.objects.create(user=request.user, task=task)
+		task_data.save()
+		fm = TasksForm()
+		return render(request, "create.html", {"fm":fm, "msg":"Task Added"})
 	else:
 		fm = TasksForm()
 		return render(request, "create.html", {"fm":fm})
 
+@login_required(login_url="user_login")
 def view(request):
-	view_tasks = TasksModel.objects.all()
+	view_tasks = TasksModel.objects.filter(user=request.user)
 	return render(request, "view.html", {"view_tasks":view_tasks})
 
 def edit(request, id):
 	edit_task = TasksModel.objects.get(task=id)
-
+	
 	fm = TasksForm(instance=edit_task)
 
 	if request.method == "POST":
 		task = TasksForm(request.POST, instance=edit_task)
-		if task.is_valid():
-			task.save()
-			fm = TasksForm()
-			return redirect("view")
-			#view_tasks = TasksModel.objects.all()
-			#return render(request, "view.html", {"view_tasks":view_tasks, "msg":"Edited"})
+		edit_task.task = request.POST['edit_task']
+		TasksModel.objects.filter(task=id).update(task=edit_task.task)
+		fm = TasksForm()
+		return redirect("view")
 
 	return render(request, "edit.html", {"fm":fm})
 
 def delete(request, id):
-	delete_task = TasksModel.objects.get(task=id)
+	delete_task = TasksModel.objects.filter(task=id).first()
 	delete_task.delete()
 	return redirect("view")
-
+	
 def feedback(request):
 	if request.method == "POST":
-		data = FbForm(request.POST)
-		if data.is_valid():
-			data.save()
-			fm = FbForm()
+		feedback_data = FeedbackForm(request.POST)
+		if feedback_data.is_valid():
+			name = request.POST.get('name')
+			email = request.POST.get('email')
+			feedback = request.POST.get('feedback')
+			feedback_data.save()
+			fm = FeedbackForm()
 			return render(request, "feedback.html", {"fm":fm, "msg":"Your feedback has been submited"})
+		else:
+			return render(request, "feedback.html", {"fm":feedback_data, "msg":"Issue"})
 	else:
-		fm = FbForm()
+		fm = FeedbackForm()
 		return render(request, "feedback.html", {"fm":fm})
