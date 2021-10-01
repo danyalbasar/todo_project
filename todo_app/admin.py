ee365def1aa3a65514a2c24a36138fa19782b9a2
Danyal Basar <dboy7098@gmail.com>
2021-10-01 12:37:23 +0530

    Update admin.py

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import TasksModel, FbModel
+from .models import TasksModel, FeedbackModel
 
 class TasksAdmin(admin.ModelAdmin):
 	list_display = ('task', 'completed', 'created')
 	list_filter = ('created', 'completed')
 
-class FbAdmin(admin.ModelAdmin):
+class FeedbackAdmin(admin.ModelAdmin):
 	list_display = ('name', 'email', 'feedback', 'created')
 	list_filter = ('created',)
 
 admin.site.register(TasksModel, TasksAdmin)
-admin.site.register(FbModel, FbAdmin)
+admin.site.register(FeedbackModel, FeedbackAdmin)
