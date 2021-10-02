e3d512efd7a00ff43df6d074e682e8e6068dad9e
Danyal Basar <dboy7098@gmail.com>
2021-10-02 14:34:22 +0530

    Update forms.py

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -4,7 +4,6 @@ from .models import TasksModel, FeedbackModel
 class TasksForm(forms.ModelForm):
 	class Meta:
 		model = TasksModel
-		widgets = {'completed': forms.CheckboxInput(attrs={'style':'width:18px; height:18px;'}),}
 		fields = '__all__'
 
 class FeedbackForm(forms.ModelForm):
