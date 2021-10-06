e8c94fa1922ca925ca3028101ebe1fae138850eb
Danyal Basar <dboy7098@gmail.com>
2021-10-06 12:50:49 +0530

    Update urls.py

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -1,8 +1,12 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 from django.contrib import admin
 from django.urls import path
 from todo_app.views import user_signup, user_login, user_forgotpass, user_logout, home, create, view, edit, delete, feedback
 
 urlpatterns = [
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path("user_signup/", user_signup, name="user_signup"),
     path("user_login/", user_login, name="user_login"),
